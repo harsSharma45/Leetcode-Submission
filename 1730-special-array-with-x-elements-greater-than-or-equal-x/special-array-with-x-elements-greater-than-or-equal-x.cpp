42534efe2f127f8b552adcc7a025fde72c27eacc
HARSH SHARMA <116057247+harsSharma45@users.noreply.github.com>
2024-05-27 17:08:16 +0530

    Time: 3 ms (40.78%) | Memory: 10.1 MB (53.24%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        int res = -1;
+        //int x = *max_element(nums.begin(),nums.end());
+
+        for(int i = 0; i < 101; i++){
+            int cnt = 0;
+            for(int j = 0; j < nums.size(); j++){
+                if(nums[j]>=i){
+                    cnt++;
+                }
+            }
+            if(cnt==i){
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+};
